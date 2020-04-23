a04be0b9628ff62c8f6cc6f36984223f37f78074
COCCHI Simon <59911613+simoncocchi@users.noreply.github.com>
2020-04-23 11:54:53 +0200

    ajout des send et de la navigation dans fichier

--- server.js
+++ server.js
@@ -17,20 +17,39 @@ app.get('/api/drive',async (req, res) => {
     }
 });
 
-app.delete('/api/drive/:name', function (req, res) {
+app.get('/api/drive/:name',async (req, res) => {
+    try {
+        const file= await fileinfo.documentContent(`test/${req.params.name}`);
+        res.send(file);
+    } catch {
+        res.send('Ne fonctionne pas');
+    }
+});
+
+app.delete('/api/drive/:name', function (req, res) { // suppresion d'un fichier ou dossier dans le temporaire
     console.log(req.params.name)
     
     fileDelete.deleteFile('test', req.params.name);
+    res.send('delete ok');
+});
+
+app.delete('/api/drive/:folder/:name', function (req, res) { // suppresion d'un fichier ou dossier dans le temporaire avec un fichier.
+    console.log(req.params.name)
+    
+    fileDelete.deleteFile(`test/${req.params.folder}`, req.params.name);
+    res.send('delete ok');
 });
 
 app.post('/api/drive', (req, res) => { // création de dossier seule 
     createDirctory.createDirectory('test',req.query.name);
     console.log("req.query.name", req.query.name)
+    res.send('creation ok');
 });
 
 app.post('/api/drive/:folder', (req, res) => { // création de dossier dans dossier
     createDirctory.createDirectory(`test/${req.params.folder}`,req.query.name);
     console.log("req.query.name", req.query.name)
+    res.send('creation ok');
 });
 
 function startServeur () {
